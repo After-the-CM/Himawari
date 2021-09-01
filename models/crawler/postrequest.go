1a0d3fad8eadf39d90d38a299cc866984eec251d
futabato <01futabato10@gmail.com>
2021-09-02 01:44:55 +0900

    refactor(crawler): 関数名、変数名、deferの削除を行いました。
    #9

--- models/crawler/postrequest.go
+++ models/crawler/postrequest.go
@@ -9,39 +9,37 @@ import (
 	"Himawari/models/entity"
 )
 
-func postRequest(r entity.RequestStruct) {
+func PostRequest(r entity.RequestStruct) {
 	// inputのurlはrequestに必要な要素を構造体or引数として渡す。
 	// 関数の中で組み合わせてurlを生成する(?)。
-	base, _ := url.Parse(r.Referer)
-	abs, _ := url.Parse(r.Path)
-	s := base.ResolveReference(abs).String()
+    base, _ := url.Parse(r.Referer)
+    rel, _ := url.Parse(r.Path)
+    abs := base.ResolveReference(rel).String()
 
 	// ここでnameの場所に値を入れる。
 	postData := r.Param
 	
-	Request, err := http.NewRequest("POST", s, strings.NewReader(postData.Encode()))
+	req, err := http.NewRequest("POST", abs, strings.NewReader(postData.Encode()))
 	if err != nil {
 		fmt.Println(err)
 	}
 	
 	// Request Header 設定
-	Request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	Request.Header.Set("User-Agent", "Himawari")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("User-Agent", "Himawari")
 	
 	// Requestを投げてResponseを得る。
 	client := new(http.Client)
-	Response, err := client.Do(Request)
-	// 関数を抜ける際に必ずResponseをCloseするようにdeferでcloseを呼ぶ。
-	defer Response.Body.Close()
-	
+	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Unable to reach the server.")
-	} else {
-		// bodyをfunc2に投げる。
-		// func2(Response)
-	}
+	} 
+	resp.Body.Close()
+	
+	// bodyをfunc2に投げる。
+	// func2(Response)
 
-	// fmt.Println("StatusCode =", Response.StatusCode)
+	// fmt.Println("StatusCode =", resp.StatusCode)
 	
 	return
 }
