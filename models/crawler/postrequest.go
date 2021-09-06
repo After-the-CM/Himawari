8d90c13a322cc8c80cc4bff4e08bfdc135688bec
futabato <01futabato10@gmail.com>
2021-09-06 23:55:19 +0900

    feat(crawler): sitemap.IsExistに対応させた。
    #9

--- models/crawler/postrequest.go
+++ models/crawler/postrequest.go
@@ -10,16 +10,14 @@ import (
 	"strings"
 
 	"Himawari/models/entity"
+	"Himawari/models/sitemap"
 )
 
 func PostRequest(r entity.RequestStruct) {
-	// inputのurlはrequestに必要な要素を構造体or引数として渡す。
-	// 関数の中で組み合わせてurlを生成する(?)。
 	base, _ := url.Parse(r.Referer)
 	rel, _ := url.Parse(r.Path)
 	abs := base.ResolveReference(rel).String()
 
-	// ここでnameの場所に値を入れる。
 	postData := r.Param
 
 	req, err := http.NewRequest("POST", abs, strings.NewReader(postData.Encode()))
@@ -27,25 +25,26 @@ func PostRequest(r entity.RequestStruct) {
 		fmt.Fprintln(os.Stderr, err)
 	}
 
-	// Request Header 設定
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("User-Agent", "Himawari")
 	req.Header.Set("Referer", r.Referer)
 
-	// Requestを投げてResponseを得る。
-	client := new(http.Client)
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to reach the server.")
+	if !sitemap.IsExist(*req) {
+		// fmt.Println("GetRequest:", req)
+		sitemap.Add(*req)
+
+		client := new(http.Client)
+		Response, err := client.Do(req)
+
+		if err != nil {
+			dump, _ := http.httputil.DumpRequestOut(Request, true)
+			fmt.Printf("%s", dump)
+			fmt.Println("Unable to reach the server.", err)
+		} else {
+			body, _ := io.ReadAll(resp.Body)
+			resp.Body.Close()
+			func2(bytes.NewBuffer(body), base)
+		}
 	}
-	
-	// fmt.Println("StatusCode =", resp.StatusCode)
-
-	body, _ := io.ReadAll(resp.Body)
-	resp.Body.Close()
-
-	// bodyをfunc2に投げる。
-	// func2(bytes.NewBuffer(body), base)
-	
 	return
 }
