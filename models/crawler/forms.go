52ab65279612c0581c2d6c2896b4fdd7f456704b
okodayon <bjrm1290142@gn.iwasaki.ac.jp>
2021-09-07 17:34:08 +0900

    fix(form.go): make run したときにエラーが出ないように条件式の変更
    32行目の条件を`-2`から`-7`へ変更
    
    #9

--- models/crawler/forms.go
+++ models/crawler/forms.go
@@ -9,17 +9,17 @@ import (
 
 // memo: Goは配列をconstとして宣言できない。
 // patternがあったとしても***Extreme***修正で対応できそう(?)
-func PrepareData() (map[string][]string) {
+func PrepareData() map[string][]string {
 	// https://developer.mozilla.org/ja/docs/Web/HTML/Element/input
 	testData := map[string][]string{
-		"email": []string{"Himawari@example.com"},
-		"url": []string{"http://example.com"},
-		"tel": []string{"00012345678", "000-1234-5678", "+81-00-1234-5678"},
-		"date": []string{"2020-12-16"},
-		"text": []string{"Himawari"},
+		"email":    []string{"Himawari@example.com"},
+		"url":      []string{"http://example.com"},
+		"tel":      []string{"00012345678", "000-1234-5678", "+81-00-1234-5678"},
+		"date":     []string{"2020-12-16"},
+		"text":     []string{"Himawari"},
 		"textarea": []string{"Himawari"},
 		//"datetime-local"
-		
+
 	}
 	return testData
 }
@@ -29,7 +29,7 @@ func SetValues(form entity.HtmlForm, r entity.RequestStruct) {
 	fmt.Println("Start func3")
 	testData := PrepareData()
 
-	for i:=0; i<len(form.Values) -2; i++ {
+	for i := 0; i < len(form.Values)-7; i++ {
 		name := form.Values["Name"][i]
 		typ := form.Values["Type"][i]
 		tag := form.Values["Tag"][i]
@@ -52,16 +52,16 @@ func SetValues(form entity.HtmlForm, r entity.RequestStruct) {
 		// patternに対応する処理は未実装
 		pattern := form.Values["Pattern"][0]
 		if pattern != "NaN" {
-			// patternFlag := true 
+			// patternFlag := true
 		}
-		
+
 		values := url.Values{}
 		values.Set("tag", tag)
 		values.Set("type", typ)
 		values.Set("name", name)
 		values.Set("value", value)
 		r.Param = values
-		
+
 		// func1へ
 		PostRequest(r)
 	}
