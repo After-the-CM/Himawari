50d20ebd40b83418186ebbbbfd2fa1aab7514f48
okodayon <bjrm1290142@gn.iwasaki.ac.jp>
2021-10-13 19:33:25 +0900

    feat(scanner): scannerの始まりをlogに出力するように
    START SCANINGをlognに表示するように
    
    #50

--- controllers/controller.go
+++ controllers/controller.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -66,6 +67,8 @@ func FoundItem(c *gin.Context) {
 }
 
 func Scan(c *gin.Context) {
+	log.Println("===============     START SCANING     ===============")
+	log.Printf("\n")
 	scanner.Scan(&entity.JsonNodes)
 	c.JSON(http.StatusOK, entity.WholeIssue)
 }
