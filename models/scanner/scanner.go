df95ae5f52067581dce8615eee83906f54234fd4
okodayon <bjrm1290142@gn.iwasaki.ac.jp>
2021-11-06 16:56:29 +0900

    feat(scanner,controller): WholeIssueのReset
    Himawariの画面でResetを押されたときにWholeIssueが空になるように。
    
    #83,#78

--- models/scanner/scanner.go
+++ models/scanner/scanner.go
@@ -20,3 +20,24 @@ func Scan(j *entity.JsonNode) {
 		}
 	}
 }
+
+func Reset() {
+	entity.WholeIssue = []entity.Issue{}
+}
+
+//再度scanを実行する際に各jsonNodeのIssueが埋まっている状態だと動かないため。
+/*
+func ResetJsonMessageOfIssue() {
+	entity.JsonNodes.Issue = []entity.Issue{}
+	resetJsonMessageOfIssue(&entity.JsonNodes)
+}
+
+func resetJsonMessageOfIssue(j *entity.JsonNode) {
+	for i, v := range j.Children {
+		if len(v.Children) != 0 {
+			resetJsonMessageOfIssue(&j.Children[i])
+		}
+		j.Children[i].Issue = []entity.Issue{}
+	}
+}
+*/
