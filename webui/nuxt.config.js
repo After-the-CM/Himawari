5956124b7e50d07986fb31280697a35bc917494d
okodayon <bjrm1290142@gn.iwasaki.ac.jp>
2021-11-19 10:31:00 +0900

    feat(plugins,modules): プラグインとモジュールを使うためのファイルを追加
    lodash,cookieを使うためにファイルの追加。また、proxyの設定はnuxt.config.js内に含まれている

--- webui/nuxt.config.js
+++ webui/nuxt.config.js
@@ -6,26 +6,24 @@ export default {
     titleTemplate: '%s - Himawari',
     title: 'Himawari',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  loading: false,
+
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [{ src: '~plugins/lodash.js' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -42,16 +40,25 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    'cookie-universal-nuxt',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    proxy: true,
+  },
 
+  // cors error 対策
+  proxy: {
+    '/api/': 'http://localhost:8080/',
+    '/report/': 'http://localhost:8080/',
+    '/sitemap/': 'http://localhost:8080/',
+  },
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -60,13 +67,12 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
