badf23fab4219aca6a13da26b451239212d8b2fc
okodayon <bjrm1290142@gn.iwasaki.ac.jp>
2021-11-19 11:07:09 +0900

    feat(pages): トップページの作成
    使用上の注意に同意しなければ進まないようにするページ
    
    #107

--- webui/pages/index.vue
+++ webui/pages/index.vue
@@ -1,77 +1,81 @@
-<template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+<template v-slot:activator="{ on, attrs }">
+  <div id="app">
+    <v-row class="pa-10" justify="center">
+      <img src="Himawari.png" />
+    </v-row>
+    <v-row justify="center">
+      <v-card elevation="24" width="700px" height="300px">
+        <v-card-title class="text-h3 justify-center">
+          使用上の注意
         </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
+        <v-card-text class="text-h5 justify-center">
+          <div class="text-center">
+            <div>許可なく他者のサーバーに使用しないでください。</div>
+            <div>違法行為となり、罰せられる可能性があります。</div>
+            <div>製作者一同は一切の責任を取りません。</div>
           </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
         </v-card-text>
         <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
+          <v-spacer></v-spacer>
+          <v-row class="mx-auto, my-5">
+            <v-btn
+              rounded
+              @click="
+                asyncData()
+                crawler()
+              "
+            >
+              同意して開始する
+            </v-btn>
+          </v-row>
         </v-card-actions>
       </v-card>
-    </v-col>
-  </v-row>
+    </v-row>
+    <v-row justify="center"> </v-row>
+  </div>
 </template>
+
+<script>
+export default {
+  asyncData({ app }) {
+    app.$cookies.remove('agree')
+  },
+  data() {
+    return {
+      theme: this.$store.state.theme,
+      dialog: true,
+      isflag: true,
+    }
+  },
+  watch: {
+    theme() {
+      this.$store.dispatch('theme', this.theme)
+      this.$vuetify.theme.dark = this.theme
+    },
+  },
+  mounted() {
+    this.$axios
+      .$get('/api/reset')
+      .then((response) => {
+        this.vuln = response
+      })
+      .catch((err) => {
+        console.log('err:', err)
+      })
+  },
+  methods: {
+    crawler() {
+      this.$router.push('crawl')
+    },
+    asyncData() {
+      this.$cookies.set('agree', 'Agree')
+    },
+    disableOn() {
+      this.isflag = true
+    },
+    disableOff() {
+      this.isflag = false
+    },
+  },
+}
+</script>
