1d63daac6896fd1daacd7ca3ea9904df9a812201
okodayon <bjrm1290142@gn.iwasaki.ac.jp>
2021-11-19 11:08:47 +0900

    feat(pages): crawlerの画面の作成
    crawlerを開始するために、パラメータ、ログインオプション等の入力欄を作成
    
    #92

---
+++ webui/pages/crawl.vue
@@ -0,0 +1,281 @@
+<template>
+  <v-app>
+    <v-card width="50%" class="mx-auto mt-5 mb-12" style="position: relative">
+      <v-toolbar color="gray" dark flat>
+        <v-toolbar-title>Crawler</v-toolbar-title>
+
+        <template v-slot:extension>
+          <v-tabs v-model="tab" centered>
+            <v-tabs-slider color="yellow"></v-tabs-slider>
+
+            <v-tab
+              v-for="item in items"
+              :key="item"
+              @click="
+                flagOn(item)
+                printFlag()
+              "
+            >
+              {{ item }}
+            </v-tab>
+          </v-tabs>
+        </template>
+      </v-toolbar>
+      <v-tabs-items v-model="tab">
+        <v-tab-item v-for="(item, i) in items" :key="i">
+          <v-card flat v-if="isflag">
+            <input-text
+              v-model="url"
+              labelText="URL"
+              :inputRule="crawlURLRule"
+              textId="url"
+            />
+
+            <login-option-switch v-model="loginflag" />
+            <v-card v-if="loginflag">
+              <v-card-title>ログイン情報入力</v-card-title>
+              <input-text
+                v-model="loginReferer"
+                labelText="LoginフォームがあるURL(Referer)"
+                textId="loginref"
+              />
+              <input-text
+                v-model="loginURL"
+                labelText="Loginリクエストの送信先"
+                textId="loginurl"
+              />
+              <v-list>
+                <v-row>
+                  <v-col cols="12">
+                    <login-option
+                      v-model="loginOptions"
+                      :methods="loginmethods"
+                      :methodcols="3"
+                    />
+                  </v-col>
+                </v-row>
+              </v-list>
+              <add-form-btn
+                :addform="loginOptions"
+                btnText="ログインパラメータ追加"
+                :adddata="{ key: '', value: '', method: 'POST' }"
+              />
+            </v-card>
+            <v-expansion-panels>
+              <v-expansion-panel>
+                <v-expansion-panel-header>
+                  Crawl時に代入するパラメーター設定はこちら
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <v-list>
+                    <v-list-item
+                      v-for="(data, index) in formdatas"
+                      :key="index"
+                      style="position: relative"
+                    >
+                      <v-text-field
+                        v-model="data.name"
+                        label="key"
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="data.value"
+                        label="value"
+                      ></v-text-field>
+
+                      <delete-form-btn
+                        :deleteform="formdatas"
+                        btnText="削除"
+                        :i="index"
+                      />
+                    </v-list-item>
+                  </v-list>
+                  <v-btn @click="addForm()">パラメーター追加</v-btn>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-card>
+          <v-card flat v-else>
+            <v-file-input
+              id="Json"
+              class="mx-5"
+              v-model="file"
+              accept=".json"
+              :loading="inputfileflag"
+              @change="finishUpload"
+              @focus="onLoding"
+              @blur="offLoding"
+            ></v-file-input>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+      <v-btn
+        v-if="isflag"
+        :disabled="url === ''"
+        rounded
+        absolute
+        right
+        class="mt-3 text-capitalize"
+        @click="doCrawl()"
+      >
+        Crawl
+      </v-btn>
+
+      <v-btn
+        v-else
+        :disabled="fileUploadFlag"
+        rounded
+        absolute
+        right
+        class="mt-3 text-capitalize"
+        @click="doUpload()"
+      >
+        Upload JSON
+      </v-btn>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+import { cloneDeep } from 'lodash'
+
+export default {
+  layout: 'original',
+  middleware({ $cookies, redirect }) {
+    if ($cookies.get('agree') !== 'Agree') {
+      return redirect('/')
+    }
+  },
+  data() {
+    return {
+      tab: null,
+      items: ['New', 'Import SitemapJson'],
+      isflag: true,
+      isURL: false,
+      url: '',
+      crawlURLRule: [(value) => !!value || '必須項目です'],
+
+      formdatas: null,
+      file: null,
+      checkedScanOption: [],
+
+      loginflag: false,
+      loginReferer: '',
+      loginURL: '',
+
+      loginOptions: null,
+      loginmethods: ['GET', 'POST'],
+
+      inputfileflag: false,
+      fileUploadFlag: true,
+    }
+  },
+  created() {
+    this.formdatas = cloneDeep(this.$store.state.crawlParams.crawlParams)
+    this.url = this.$store.state.crawlURL.crawlURL
+
+    this.loginReferer = this.$store.state.loginPath.loginRef
+    this.loginURL = this.$store.state.loginPath.loginURL
+
+    this.loginOptions = cloneDeep(this.$store.state.loginParams.loginParams)
+  },
+  methods: {
+    flagOn(item) {
+      if (item === 'New') {
+        this.isflag = true
+      } else if (item === 'Import SitemapJson') {
+        this.isflag = false
+        // New から Sitemap Input のページに切り替わった際にログインオプションの欄をOFFに
+        this.loginflag = false
+      }
+    },
+    printFlag() {
+      console.log('ok')
+    },
+    doCrawl() {
+      this.$store.commit('crawlParams/changecrawlParams', this.formdatas)
+      this.$store.commit('crawlURL/changecrawlURL', this.url)
+
+      const forms = new FormData()
+
+      forms.append('url', this.url)
+
+      if (this.loginflag) {
+        forms.append('loginReferer', this.loginReferer)
+        this.$store.commit('loginPath/changeloginRef', this.loginReferer)
+
+        forms.append('loginURL', this.loginURL)
+        this.$store.commit('loginPath/changeloginURL', this.loginURL)
+
+        this.$store.commit('loginParams/changeloginParams', this.loginOptions)
+
+        for (const i in this.loginOptions) {
+          forms.append('loginKey[]', this.loginOptions[i].key)
+          console.log(this.loginOptions[i].key)
+          forms.append('loginValue[]', this.loginOptions[i].value)
+          forms.append('loginMethod[]', this.loginOptions[i].method)
+        }
+      }
+
+      for (const j in this.formdatas) {
+        // names.push(this.formdatas[i].name)
+        forms.append('name[]', this.formdatas[j].name)
+        forms.append('value[]', this.formdatas[j].value)
+      }
+
+      this.$axios
+        .$post('/api/crawl', forms)
+        .then((response) => {
+          console.log(this.url)
+          console.log(response)
+
+          this.transitionsitemap()
+        })
+        .catch((err) => {
+          console.log('err:', err)
+        })
+      // const params = new URLSearchParams()
+    },
+    doUpload() {
+      const data = new FormData()
+      data.append('sitemap', this.file, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      this.$axios
+        .$post('/sitemap/upload', data)
+        .then((response) => {
+          console.log(this.url)
+          console.log(response)
+          this.transitionsitemap()
+        })
+        .catch((err) => {
+          console.log('err:', err)
+        })
+    },
+    addForm() {
+      const adddata = {
+        name: '',
+        value: '',
+      }
+      this.formdatas.push(adddata)
+    },
+    deleteForm(index) {
+      this.formdatas.splice(index, 1)
+    },
+    onLoding() {
+      this.inputfileflag = true
+    },
+    offLoding() {
+      this.inputfileflag = false
+    },
+    finishUpload() {
+      this.inputfileflag = false
+      this.fileUploadFlag = false
+    },
+    transitionsitemap() {
+      this.$router.push('/scan')
+    },
+  },
+}
+</script>
