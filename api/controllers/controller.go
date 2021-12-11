a29cf2ec8c6ac18175ad49c74fd36bdd05ed296e
y0d3n <bjay1290115@gn.iwasaki.ac.jp>
2021-12-12 01:19:19 +0900

    feat(scanner): スキャン時間の概算。だいたいのスキャン時間を予想します。

--- api/controllers/controller.go
+++ api/controllers/controller.go
@@ -128,6 +128,7 @@ func Scan(c *gin.Context) {
 		scanner.SetGenLandmark(formdata.LandmarkNumber)
 	}
 
+	scanner.CalcApproximateTime(&entity.JsonNodes)
 	scanner.Scan(&entity.JsonNodes)
 
 	scanflag = "finished"
